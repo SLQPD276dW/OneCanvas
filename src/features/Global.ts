5d61f792bf1d7be53c78ff5e175ea98ece085ecb
SLQPD276dW <51816533+SLQPD276dW@users.noreply.github.com>
2022-03-05 23:04:42 +0900

    doc: Global.tsへコメントを追加
    Global.tsの関数や変数に分かりやすいようにコメントを追加

--- src/features/Global.ts
+++ src/features/Global.ts
@@ -1,4 +1,12 @@
-export function CheckNullOrUndefined<T>(variable: T): asserts variable is NonNullable<T>{
+/**
+ * Global.ts
+ */
+
+/**
+ * variableからnullableを除去する関数
+ * @param variable nullableを除去する変数
+ */
+ export function CheckNullOrUndefined<T>(variable: T): asserts variable is NonNullable<T>{
     if (variable === undefined || variable === null) {
         throw new Error(
             `variableは${variable}です。`
@@ -6,4 +14,5 @@ export function CheckNullOrUndefined<T>(variable: T): asserts variable is NonNul
     }
 }
 
+// ソースコードが置いてあるGithubリポジトリへのリンク
 export const repository_link = "https://github.com/SLQPD276dW/OneCanvas";
